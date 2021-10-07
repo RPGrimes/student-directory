a5bfc83c0f31a15a2a200e24e1be1ef76f4685cb
Ryan Grimes <ryanpaulgrimes@gmail.com>
2021-10-07 13:38:42 +0200

    Add methods to code to make code more legible

--- directory.rb
+++ directory.rb
@@ -1,4 +1,3 @@
-student_count = 11
 #let's put all students into an array
 students = [
  "Dr. Hannibal Lecter",
@@ -11,12 +10,26 @@ students = [
  "Freddy Krueger",
  "The Joker",
  "Joffrey Baratheon",
- "Norman Bates"]
-# and then print them
-puts "The students of Villains Academy"
-puts "_____________"
-students.each do |student|
-  puts student
-end 
-#finally, we print the total
-puts "Overall, we have #{students.count} great students"
+ "Norman Bates"
+]
+
+#create methods to tidy code
+def print_header
+  puts "The students of Villains Academy"
+  puts "_____________"
+end
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+#call methods
+print_header
+print(students)
+print_footer(students)
